ced3b2ba38d745f856bfd1bd87261f4ed84eae01
marutiss <235kiki@naver.com>
2022-07-08 16:13:06 +0900

    제품데이터 교체 및 수정

--- src/App.vue
+++ src/App.vue
@@ -23,40 +23,33 @@
     
     <!-- <h4 >{{products[i]}}</h4>
     <p> 50만원</p>
-   <button @click="increase">허위매물신고</button> <span>신고수 : {{신고수[i]}}</span> -->
+   <button @click="increase">추천!</button> <span>추천수 : {{추천수[i]}}</span> -->
   </div>
 
- <div>
-  <img src="./assets/room0.jpg" class="room-img">
-  <h4 @click="모달창open=true">{{products[0]}}</h4>
-    <p> 50만원</p>
-   <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
+ <div v-for="(x,i) in 6" :key="i">
+  <img :src="원룸들[i].image" class="room-img">
+  <h4 @click="모달창open=true">{{원룸들[i].title}}</h4>
+    <p> {{원룸들[i].price}}원</p>
+   <button @click="추천수[i]++">추천!</button> <span>추천수 : {{추천수[i]}}</span>
  </div>
-<div>
-  <img src="./assets/room1.jpg" class="room-img">
-  <h4 >{{products[1]}}</h4>
-    <p> 50만원</p>
-   <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
- </div>
-<div>
-  <img src="./assets/room2.jpg" class="room-img">
-  <h4 >{{products[2]}}</h4>
-    <p> 50만원</p>
-   <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
- </div>
+
+
+
 </template>
 
 <script>
 
-
-
+import room from './assets/roomdata.js';
+room;
+//import {변수,변수} from './assets/roomdata.js';
 
 export default {
   name: 'App',
   data(){
     return{
+      원룸들 : room,
       모달창open : false, 
-      신고수:[0,0,0],
+      추천수:[0,0,0,0,0,0],
      메뉴들: ['Home','Shop','About'],
      products :['역삼동 원룸','천호동 원룸','마포구 원룸'],
     }
@@ -64,7 +57,7 @@ export default {
 
   methods:{
     increase(){
-    this.신고수++;
+    this.추천수++;
     }
   },
 
